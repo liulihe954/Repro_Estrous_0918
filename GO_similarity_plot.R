c35e79c72249a6d265da9a7e951c2409e7b053ba
Lihe Liu <lihe.liu@ufl.edu>
2019-09-24 13:49:55 -0400

    first cor plot

--- GO_similarity_plot.R
+++ GO_similarity_plot.R
@@ -13,6 +13,7 @@ corrplot(goSimMat, tl.col = "black", tl.cex = 0.8, method = "shade", order = "hc
 # dataset pre
 # BiocManager::install("org.Bt.eg.db")
 library("org.Bt.eg.db")
+
 semData_BP <- godata('org.Bt.eg.db', ont="BP", computeIC=T) #ont="BP"
 semData_MF <- godata('org.Bt.eg.db', ont="MF", computeIC=T) #ont="BP"
 semData_CC <- godata('org.Bt.eg.db', ont="CC", computeIC=T) #ont="BP"
@@ -26,12 +27,45 @@ AR_CNTRL_enrich_CC_List = dplyr::filter(AR_CNTRL_enrich,namespace_1003 == "cellu
 AR_CNTRL_enrich_MF_List = dplyr::filter(AR_CNTRL_enrich,namespace_1003 == "molecular_function") %>% 
   dplyr::select(go_id) %>% unlist();attributes(AR_CNTRL_enrich_MF_List) = NULL
 
-goSim("GO:0055114","GO:0120162",semData=semData_BP,measure="Jiang")
+#goSim("GO:0055114","GO:0120162",semData=semData_BP,measure="Jiang")
 
 goSimMat_test = mgoSim(AR_CNTRL_enrich_BP_List,
                        AR_CNTRL_enrich_BP_List,
                        semData=semData_BP,
                        measure="Jiang", combine=NULL) # combind why use null?
+suspectID = rownames(goSimMat_test)[is.na(goSimMat_test[,1])]
+suspectID
+
+AR_CNTRL_enrich_BP_List_new = AR_CNTRL_enrich_BP_List[-which(AR_CNTRL_enrich_BP_List %in% suspectID)]
+goSimMat_test_new = mgoSim(AR_CNTRL_enrich_BP_List_new,
+                           AR_CNTRL_enrich_BP_List_new,
+                           semData=semData_BP,
+                           measure="Jiang", combine=NULL) # combind why use null?
+
+x = corrplot(goSimMat_test_new , tl.col = "black", tl.cex = 0.8, 
+             method = "shade", order = "hclust", 
+             hclust.method = "centroid", is.corr = FALSE)
+x
+
+
+
+
+
+goSimMat_test[rownames(goSimMat_test) == "GO:0120162",]
+
+1] "GO:0033089" "GO:0032534"
+[3] "GO:0070120" "GO:0048843"
+[5] "GO:1905606" "GO:0120162"
+
+
+
+
+
+select(org.Bt.eg.db,keys ="GO:1905606",keytype = "GO","ENTREZID" )
+
+
+view(goSimMat_test)
+
 view(goSimMat_test)
 goSimMat_test2 = mgoSim(list,
                        list,
