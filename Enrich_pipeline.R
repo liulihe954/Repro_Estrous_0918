a9c8a2a31e36e72cab72299d10f3ccea2ae7cf3d
Lihe Liu <lihe.liu@ufl.edu>
2019-09-26 18:36:12 -0400

    add family info for enrichment

--- Enrich_pipeline.R
+++ Enrich_pipeline.R
@@ -152,30 +152,40 @@ Interpro_Enrich_Results_thres005 =
 
 ## load in the results just created. Tip: use the key word above
 load("Interpro_Enrichment_thres005_0925.RData")
+
+########
+library(tidyverse)
+match_family = read.table("entry.list",sep = "\t",header = T)
+match_family = as_tibble(match_family) %>% dplyr::select(ENTRY_AC,ENTRY_TYPE) %>% 
+  rename(InterproID = ENTRY_AC,Type =ENTRY_TYPE)
+
+
 # parse 1 by 1, and attach the space name in the end
 compile_select_index = c("InterproID","Interpro_Name","Total_Genes","Significant_Genes","pvalue","hitsPerc")
 ### group 1
-AR_CNTRL_enrich_IP = Parse_Interpro_Results(Interpro_results_b[2])
+AR_CNTRL_enrich_IP = Parse_Interpro_Results(Interpro_results_b[2]) 
 names(AR_CNTRL_enrich_IP) = c("InterproID","Interpro_Name","Total_Genes","Significant_Genes","pvalue","ExternalLoss_total","InternalLoss_sig","hitsPerc")
-AR_CNTRL_enrich_IP = dplyr::select(AR_CNTRL_enrich_IP,compile_select_index)
+AR_CNTRL_enrich_IP = dplyr::select(AR_CNTRL_enrich_IP,compile_select_index) %>% dplyr::left_join(match_family,by=c("InterproID" = "InterproID"))
 #
 PRF_CNTRL_enrich_IP = Parse_Interpro_Results(Interpro_results_b[3])
 names(PRF_CNTRL_enrich_IP) = c("InterproID","Interpro_Name","Total_Genes","Significant_Genes","pvalue","ExternalLoss_total","InternalLoss_sig","hitsPerc")
-PRF_CNTRL_enrich_IP   = dplyr::select(PRF_CNTRL_enrich_IP,compile_select_index)
+PRF_CNTRL_enrich_IP  = dplyr::select(PRF_CNTRL_enrich_IP,compile_select_index) %>% dplyr::left_join(match_family,by=c("InterproID" = "InterproID"))
+
 ### group 2
 FPM_CNTRL_enrich_IP = Parse_Interpro_Results(Interpro_results_b[1])
 names(FPM_CNTRL_enrich_IP) = c("InterproID","Interpro_Name","Total_Genes","Significant_Genes","pvalue","ExternalLoss_total","InternalLoss_sig","hitsPerc")
-FPM_CNTRL_enrich_IP = dplyr::select(FPM_CNTRL_enrich_IP,compile_select_index)
+FPM_CNTRL_enrich_IP = dplyr::select(FPM_CNTRL_enrich_IP,compile_select_index)%>% dplyr::left_join(match_family,by=c("InterproID" = "InterproID"))
+
 #
 SMP_CNTRL_enrich_IP= Parse_Interpro_Results(Interpro_results_b[4])
 names(SMP_CNTRL_enrich_IP) = c("InterproID","Interpro_Name","Total_Genes","Significant_Genes","pvalue","ExternalLoss_total","InternalLoss_sig","hitsPerc")
-SMP_CNTRL_enrich_IP = dplyr::select(SMP_CNTRL_enrich_IP,compile_select_index) 
+SMP_CNTRL_enrich_IP = dplyr::select(SMP_CNTRL_enrich_IP,compile_select_index) %>% dplyr::left_join(match_family,by=c("InterproID" = "InterproID"))
 #
 SMP_FMP_enrich_IP = Parse_Interpro_Results(Interpro_results_b[5])
 names(SMP_FMP_enrich_IP) = c("InterproID","Interpro_Name","Total_Genes","Significant_Genes","pvalue","ExternalLoss_total","InternalLoss_sig","hitsPerc")
-SMP_FMP_enrich_IP =  dplyr::select(SMP_FMP_enrich_IP,compile_select_index)
+SMP_FMP_enrich_IP =  dplyr::select(SMP_FMP_enrich_IP,compile_select_index)%>% dplyr::left_join(match_family,by=c("InterproID" = "InterproID"))
 
-########
+
 #### group 1 - regression
 Interpro_Results_full_005_reg <-
   dplyr::full_join(AR_CNTRL_enrich_IP,PRF_CNTRL_enrich_IP,by = c("InterproID" = "InterproID")) %>% 
@@ -191,11 +201,14 @@ Interpro_Results_full_005_preg <-
 Interpro_Results_inner_005_preg <-  
   dplyr::inner_join(FPM_CNTRL_enrich_IP,SMP_CNTRL_enrich_IP,by = c("InterproID" = "InterproID")) %>% 
   dplyr::inner_join(SMP_FMP_enrich_IP,by = c("InterproID" = "InterproID"))
+
+
 require(openxlsx)
 Interpro_Enrich_Regression_005 <- list("Full_join" = Interpro_Results_full_005_reg, "Inner_join" = Interpro_Results_inner_005_reg)
-write.xlsx(Interpro_Enrich_Regression_005,file = "Interpro_Enrich_Regression_005_0925.xlsx")
+write.xlsx(Interpro_Enrich_Regression_005,file = "Interpro_Enrich_Regression_005_0925_withFamily.xlsx")
 Interpro_Enrich_Pregnancy_005 <- list("Full_join" = Interpro_Results_full_005_preg, "Inner_join" = Interpro_Results_inner_005_preg)
-write.xlsx(Interpro_Enrich_Pregnancy_005,file = "Interpro_Enrich_Pregnancy_005_0925.xlsx")
+write.xlsx(Interpro_Enrich_Pregnancy_005,file = "Interpro_Enrich_Pregnancy_005_0925_withFamily.xlsx")
+
 
 
 
