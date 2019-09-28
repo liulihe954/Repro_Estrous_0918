7e4b5ffa1fc7daf9ecf1071621a871ba6d5ba634
Lihe Liu <lihe.liu@ufl.edu>
2019-09-28 11:09:58 -0400

    figure out the missing family info in entry.list

--- Enrich_pipeline.R
+++ Enrich_pipeline.R
@@ -157,7 +157,7 @@ load("Interpro_Enrichment_thres005_0925.RData")
 library(tidyverse)
 match_family = read.table("entry.list",sep = "\t",header = T)
 match_family = as_tibble(match_family) %>% dplyr::select(ENTRY_AC,ENTRY_TYPE) %>% 
-  rename(InterproID = ENTRY_AC,Type =ENTRY_TYPE)
+rename(InterproID = ENTRY_AC,Type =ENTRY_TYPE)
 
 
 # parse 1 by 1, and attach the space name in the end
@@ -165,49 +165,65 @@ compile_select_index = c("InterproID","Interpro_Name","Total_Genes","Significant
 ### group 1
 AR_CNTRL_enrich_IP = Parse_Interpro_Results(Interpro_results_b[2]) 
 names(AR_CNTRL_enrich_IP) = c("InterproID","Interpro_Name","Total_Genes","Significant_Genes","pvalue","ExternalLoss_total","InternalLoss_sig","hitsPerc")
-AR_CNTRL_enrich_IP = dplyr::select(AR_CNTRL_enrich_IP,compile_select_index) %>% dplyr::left_join(match_family,by=c("InterproID" = "InterproID"))
+AR_CNTRL_enrich_IP = dplyr::select(AR_CNTRL_enrich_IP,compile_select_index)  #%>% dplyr::left_join(match_family,by=c("InterproID" = "InterproID"))
+
+length(AR_CNTRL_enrich_IP$InterproID) - length(AR_CNTRL_enrich_IP$InterproID %in% match_family$InterproID)
+length(AR_CNTRL_enrich_IP$InterproID) - length(intersect(AR_CNTRL_enrich_IP$InterproID, match_family$InterproID))
+
+AR_CNTRL_enrich_IP = merge(AR_CNTRL_enrich_IP,match_family,by = "InterproID")
+#View(AR_CNTRL_enrich_IP)
+dim(AR_CNTRL_enrich_IP)
+
 #
 PRF_CNTRL_enrich_IP = Parse_Interpro_Results(Interpro_results_b[3])
 names(PRF_CNTRL_enrich_IP) = c("InterproID","Interpro_Name","Total_Genes","Significant_Genes","pvalue","ExternalLoss_total","InternalLoss_sig","hitsPerc")
-PRF_CNTRL_enrich_IP  = dplyr::select(PRF_CNTRL_enrich_IP,compile_select_index) %>% dplyr::left_join(match_family,by=c("InterproID" = "InterproID"))
+PRF_CNTRL_enrich_IP  = dplyr::select(PRF_CNTRL_enrich_IP,compile_select_index) #%>% dplyr::left_join(match_family,by=c("InterproID" = "InterproID"))
+PRF_CNTRL_enrich_IP = merge(PRF_CNTRL_enrich_IP,match_family,by = "InterproID")
 
 ### group 2
 FPM_CNTRL_enrich_IP = Parse_Interpro_Results(Interpro_results_b[1])
 names(FPM_CNTRL_enrich_IP) = c("InterproID","Interpro_Name","Total_Genes","Significant_Genes","pvalue","ExternalLoss_total","InternalLoss_sig","hitsPerc")
-FPM_CNTRL_enrich_IP = dplyr::select(FPM_CNTRL_enrich_IP,compile_select_index)%>% dplyr::left_join(match_family,by=c("InterproID" = "InterproID"))
-
+FPM_CNTRL_enrich_IP = dplyr::select(FPM_CNTRL_enrich_IP,compile_select_index) # %>% dplyr::left_join(match_family,by=c("InterproID" = "InterproID"))
+FPM_CNTRL_enrich_IP = merge(FPM_CNTRL_enrich_IP,match_family,by = "InterproID")
 #
 SMP_CNTRL_enrich_IP= Parse_Interpro_Results(Interpro_results_b[4])
 names(SMP_CNTRL_enrich_IP) = c("InterproID","Interpro_Name","Total_Genes","Significant_Genes","pvalue","ExternalLoss_total","InternalLoss_sig","hitsPerc")
-SMP_CNTRL_enrich_IP = dplyr::select(SMP_CNTRL_enrich_IP,compile_select_index) %>% dplyr::left_join(match_family,by=c("InterproID" = "InterproID"))
+SMP_CNTRL_enrich_IP = dplyr::select(SMP_CNTRL_enrich_IP,compile_select_index) #%>% dplyr::left_join(match_family,by=c("InterproID" = "InterproID"))
+SMP_CNTRL_enrich_IP = merge(SMP_CNTRL_enrich_IP,match_family,by = "InterproID")
+
 #
 SMP_FMP_enrich_IP = Parse_Interpro_Results(Interpro_results_b[5])
 names(SMP_FMP_enrich_IP) = c("InterproID","Interpro_Name","Total_Genes","Significant_Genes","pvalue","ExternalLoss_total","InternalLoss_sig","hitsPerc")
-SMP_FMP_enrich_IP =  dplyr::select(SMP_FMP_enrich_IP,compile_select_index)%>% dplyr::left_join(match_family,by=c("InterproID" = "InterproID"))
+SMP_FMP_enrich_IP =  dplyr::select(SMP_FMP_enrich_IP,compile_select_index) #%>% dplyr::left_join(match_family,by=c("InterproID" = "InterproID"))
+SMP_FMP_enrich_IP = merge(SMP_FMP_enrich_IP,match_family,by = "InterproID")
 
 
 #### group 1 - regression
 Interpro_Results_full_005_reg <-
-  dplyr::full_join(AR_CNTRL_enrich_IP,PRF_CNTRL_enrich_IP,by = c("InterproID" = "InterproID")) %>% 
-  tidyr::replace_na(list(InterproID.x = "Not Found",InterproID.y = "Not Found")) 
+  dplyr::full_join(AR_CNTRL_enrich_IP,PRF_CNTRL_enrich_IP,
+                   by = c("InterproID" = "InterproID")) 
+#%>% tidyr::replace_na(list(InterproID.x = "Not Found",InterproID.y = "Not Found")) 
 Interpro_Results_inner_005_reg <-  
-  dplyr::inner_join(AR_CNTRL_enrich_IP,PRF_CNTRL_enrich_IP,by = c("InterproID" = "InterproID"))
+  dplyr::inner_join(AR_CNTRL_enrich_IP,PRF_CNTRL_enrich_IP,
+                    by = c("InterproID" = "InterproID"))
 #### group 2 - pregnancy
 Interpro_Results_full_005_preg <-  
-  dplyr::full_join(FPM_CNTRL_enrich_IP,SMP_CNTRL_enrich_IP,by = c("InterproID" = "InterproID")) %>% 
-  dplyr::full_join(SMP_FMP_enrich_IP,by = c("InterproID" = "InterproID")) %>% 
-  tidyr::replace_na(list(InterproID.x = "Not Found",InterproID.y = "Not Found",InterproID = "Not Found"))
-
+  dplyr::full_join(FPM_CNTRL_enrich_IP,SMP_CNTRL_enrich_IP,
+                   by = c("InterproID" = "InterproID")) %>% 
+  dplyr::full_join(SMP_FMP_enrich_IP,
+                   by = c("InterproID" = "InterproID")) 
+ # %>%  tidyr::replace_na(list(InterproID.x = "Not Found",InterproID.y = "Not Found",InterproID = "Not Found"))
 Interpro_Results_inner_005_preg <-  
-  dplyr::inner_join(FPM_CNTRL_enrich_IP,SMP_CNTRL_enrich_IP,by = c("InterproID" = "InterproID")) %>% 
+  dplyr::inner_join(FPM_CNTRL_enrich_IP,SMP_CNTRL_enrich_IP, 
+                    by = c("InterproID" = "InterproID")) %>% 
   dplyr::inner_join(SMP_FMP_enrich_IP,by = c("InterproID" = "InterproID"))
 
 
 require(openxlsx)
 Interpro_Enrich_Regression_005 <- list("Full_join" = Interpro_Results_full_005_reg, "Inner_join" = Interpro_Results_inner_005_reg)
-write.xlsx(Interpro_Enrich_Regression_005,file = "Interpro_Enrich_Regression_005_0925_withFamily.xlsx")
+write.xlsx(Interpro_Enrich_Regression_005,file = "Interpro_Enrich_Regression_005_0925_withFamily2.xlsx")
 Interpro_Enrich_Pregnancy_005 <- list("Full_join" = Interpro_Results_full_005_preg, "Inner_join" = Interpro_Results_inner_005_preg)
-write.xlsx(Interpro_Enrich_Pregnancy_005,file = "Interpro_Enrich_Pregnancy_005_0925_withFamily.xlsx")
+write.xlsx(Interpro_Enrich_Pregnancy_005,file = "Interpro_Enrich_Pregnancy_005_0925_withFamily2.xlsx")
 
 
 
--- Function_Source.R
+++ Function_Source.R
@@ -231,29 +231,7 @@ Parse__Results = function(KEGG_results_b){
 }
 
 
-Parse_GO_Results = function(GO_results_b){
-  all_enrich_GO = data.frame(ID=character(),
-                             Description=character(),
-                             GeneRatio=character(),
-                             BgRatio=character(),
-                             pvalue=numeric(),
-                             p.adjust=numeric(),
-                             qvalue=numeric(),
-                             geneID=character(),
-                             Count=numeric(),
-                             stringsAsFactors=FALSE)
-  for (i in 1:length(GO_results_b)){
-    len = dim(data.frame(GO_results_b[i]))[1]
-    if (len> 0){
-      all_enrich_GO = rbind(all_enrich_GO,data.frame(GO_results_b[i]))
-    }
-  }
-  #all_enrich_KEGG <- all_enrich_KEGG %>% dplyr::group_by(KEGG.ID) %>% dplyr::distinct()
-  total_hits = dim(all_enrich_GO)[1]
-  total_modules = length(GO_results_b)
-  print(paste(total_hits,"hits found in",total_modules,"non-preserved modules"))
-  return(ParseResults = all_enrich_GO)
-}
+
 
 
 #########################################################################################################################
