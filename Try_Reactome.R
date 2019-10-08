338bc5806fecbe3024a44ab83f49e487f57699fb
Lihe Liu <lihe.liu@ufl.edu>
2019-10-08 16:51:57 -0400

    finish reactome

--- Try_Reactome.R
+++ Try_Reactome.R
@@ -220,4 +220,79 @@ Reactome_Enrich_all_path_1001 = Reactome_Enrich(total_genes_all=Total_list_out_e
                                                 keyword = "Reactome_Enrich_all_path_1008")
 
 #########################################################################################################################
+#total_genes_all=Total_list_out_entrez
+#sig_genes_all=Sig_list_out_entrez
+#InputSource=  NCBI2Reactome_all_react_bt
+
+#(dplyr::filter(InputSource,ProteinID == "R-BTA-72185"))
+#table(duplicated(InputSource$Protein_Description))
+
+##############################
+### formating the results  ##
+##############################
+load("Reactome_Enrich_lowest_path_1008.RData")
+keyword1 = "Reactome_Enrich_Regression_005_1008_lowest_path.xlsx"
+keyword2 = "Reactome_Enrich_Pregnancy_005_1008_lowest_path.xlsx"
+
+load("Reactome_Enrich_all_path_1008.RData")
+keyword1 = "Reactome_Enrich_Regression_005_1008_all_path.xlsx"
+keyword2 = "Reactome_Enrich_Pregnancy_005_1008_all_path.xlsx"
+
+load("Reactome_Enrichment_all_react_1008.RData")
+keyword1 = "Reactome_Enrich_Regression_005_1008_all_react.xlsx"
+keyword2 = "Reactome_Enrich_Pregnancy_005_1008_all_react.xlsx"
+
+
+# parse 1 by 1, and attach the space name in the end
+compile_select_index = c("ReactomeID","ReactomeTerm","Total_Genes","Significant_Genes","pvalue","hitsPerc")
+### group 1
+AR_CNTRL_enrich_KEGG = Parse_Results(Reactome_results_b[2]) 
+names(AR_CNTRL_enrich_KEGG) = c("ReactomeID","ReactomeTerm","Total_Genes","Significant_Genes","pvalue","ExternalLoss_total","InternalLoss_sig","hitsPerc")
+AR_CNTRL_enrich_KEGG = dplyr::select(AR_CNTRL_enrich_KEGG,compile_select_index)  #%>% dplyr::left_join(match_family,by=c("InterproID" = "InterproID"))
+#
+PRF_CNTRL_enrich_KEGG = Parse_Results(Reactome_results_b[3])
+names(PRF_CNTRL_enrich_KEGG) = c("ReactomeID","ReactomeTerm","Total_Genes","Significant_Genes","pvalue","ExternalLoss_total","InternalLoss_sig","hitsPerc")
+PRF_CNTRL_enrich_KEGG  = dplyr::select(PRF_CNTRL_enrich_KEGG,compile_select_index) #%>% dplyr::left_join(match_family,by=c("InterproID" = "InterproID"))
+### group 2
+FPM_CNTRL_enrich_KEGG = Parse_Results(Reactome_results_b[1])
+names(FPM_CNTRL_enrich_KEGG) = c("ReactomeID","ReactomeTerm","Total_Genes","Significant_Genes","pvalue","ExternalLoss_total","InternalLoss_sig","hitsPerc")
+FPM_CNTRL_enrich_KEGG = dplyr::select(FPM_CNTRL_enrich_KEGG,compile_select_index) # %>% dplyr::left_join(match_family,by=c("InterproID" = "InterproID"))
+#
+SMP_CNTRL_enrich_KEGG= Parse_Results(Reactome_results_b[4])
+names(SMP_CNTRL_enrich_KEGG) = c("ReactomeID","ReactomeTerm","Total_Genes","Significant_Genes","pvalue","ExternalLoss_total","InternalLoss_sig","hitsPerc")
+SMP_CNTRL_enrich_KEGG = dplyr::select(SMP_CNTRL_enrich_KEGG,compile_select_index) #%>% dplyr::left_join(match_family,by=c("InterproID" = "InterproID"))
+#
+SMP_FMP_enrich_KEGG = Parse_Results(Reactome_results_b[5])
+names(SMP_FMP_enrich_KEGG) = c("ReactomeID","ReactomeTerm","Total_Genes","Significant_Genes","pvalue","ExternalLoss_total","InternalLoss_sig","hitsPerc")
+SMP_FMP_enrich_KEGG =  dplyr::select(SMP_FMP_enrich_KEGG,compile_select_index) #%>% dplyr::left_join(match_family,by=c("InterproID" = "InterproID"))
+
+#### group 1 - regression
+KEGG_Results_full_005_reg <-
+  dplyr::full_join(AR_CNTRL_enrich_KEGG,PRF_CNTRL_enrich_KEGG,
+                   by = c("ReactomeID" = "ReactomeID")) 
+#%>% tidyr::replace_na(list(InterproID.x = "Not Found",InterproID.y = "Not Found")) 
+KEGG_Results_inner_005_reg <-  
+  dplyr::inner_join(AR_CNTRL_enrich_KEGG,PRF_CNTRL_enrich_KEGG,
+                    by = c("ReactomeID" = "ReactomeID")) 
+
+#### group 2 - pregnancy
+KEGG_Results_full_005_preg <-  
+  dplyr::full_join(FPM_CNTRL_enrich_KEGG,SMP_CNTRL_enrich_KEGG,
+                   by = c("ReactomeID" = "ReactomeID"))  %>% 
+  dplyr::full_join(SMP_FMP_enrich_KEGG,
+                   by = c("ReactomeID" = "ReactomeID"))  
+# %>%  tidyr::replace_na(list(InterproID.x = "Not Found",InterproID.y = "Not Found",InterproID = "Not Found"))
+KEGG_Results_inner_005_preg <-  
+  dplyr::inner_join(FPM_CNTRL_enrich_KEGG,SMP_CNTRL_enrich_KEGG, 
+                    by = c("ReactomeID" = "ReactomeID"))  %>% 
+  dplyr::inner_join(SMP_FMP_enrich_KEGG,by = c("ReactomeID" = "ReactomeID")) 
+
+require(openxlsx)
+KEGG_Enrich_Regression_005 <- list("Full_join" = KEGG_Results_full_005_reg, "Inner_join" = KEGG_Results_inner_005_reg)
+write.xlsx(KEGG_Enrich_Regression_005,file = keyword1)
+KEGG_Enrich_Pregnancy_005 <- list("Full_join" = KEGG_Results_full_005_preg, "Inner_join" = KEGG_Results_inner_005_preg)
+write.xlsx(KEGG_Enrich_Pregnancy_005,file = keyword2)
+
+
+
 
